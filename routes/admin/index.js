81993f406f61be43d0dd1f98d55a17c196942214
kangcodeis <miiren543@damu.net>
2022-04-04 16:58:53 +0900

    admin유저리스트 html수정 라우터작성 db 추가

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,32 +1,99 @@
 const express = require('express')
 const router = express.Router()
+
 const {pool} =require('../../db')
 
-router.get('/',(req,res)=>{ 
-    // localhost:4000/admin
-    res.render('admin/admin.html')
+
+
+// router.get('/',(req,res)=>{ 
+//     res.render('admin/admin.html')
+// })
+
+// router.post('/', async(req,res)=>{
+//     const {userid,userpw} = req.body
+//     console.log(req.body)
+//     const conn = await pool.getConnection()
+//     try {
+//         const sql = `SELECT * FROM user WHERE userid = '${userid}' AND userpw= '${userpw}' AND level='1'`
+//         const [result] = await conn.query(sql);
+//         if (result.length !==0) {
+//             req.session.userid = result[0].userid
+//             res.redirect('admin/list')
+//         } else {
+//             res.send(alertmove('admin','Check your ID or Password'))
+//         }
+//     } catch (error) {
+//         console.log(error)
+//     } finally {
+//         conn.release()
+//     }
+// })
+
+router.get('/',(req,res)=>{
+    const { user } = req.session
+    if (user !== undefined) {
+        res.send(alertmove('/', '접근할 수 없습니다.'))
+    } else {
+        res.render('admin/admin.html')
+    }
 })
 
-router.post('/', async(req,res)=>{
-    const {userid,userpw} = req.body
-    console.log(req.body)
-    const conn = await pool.getConnection()
+// router.post('/',async(req,res)=>{
+//     const { userid, userpw } = req.body;
+//     const conn = await pool.getConnection();
+//     try {
+//         const sql = `SELECT * FROM user WHERE userid = "${userid}" AND userpw = "${userpw}"`
+//         const [result] = await conn.query(sql)
+//         if (result.length !== 0) {
+//             if (result[0].level > 2) {
+//                 res.send(alertmove('/admin', '접근권한이 없습니다.'));
+//             } else {
+//                 req.session.admin = result[0];
+//                 res.redirect('/')
+//             }
+//         } else {
+//             res.send(alertmove('/admin', '존재하지 않는 계정입니다.'));
+//         }
+
+//     } catch(error){
+//         console.log(error)
+//     } finally {
+//         conn.release()
+//     }
+    
+// })   
+
+router.get('/userList',async(req,res)=>{
+    const conn = await pool.getConnection();
     try {
-        const sql = `SELECT * FROM user WHERE userid = '${userid}' AND userpw= '${userpw}' AND level='1'`
-        const [result] = await conn.query(sql);
-        if (result.length !==0) {
-            req.session.userid = result[0].userid
-            res.redirect('admin/list')
-        } else {
-            res.send(alertmove('admin','Check your ID or Password'))
-        }
-    } catch (error) {
+        const[content] = await conn.query(
+            `SELECT level, userid, username, nickname,
+                    birth, address, gender, tell,
+                    email
+                    FROM user`
+        )
+        res.render('admin/user_list',{content})
+    } catch (error){
         console.log(error)
     } finally {
-        conn.release()
+        conn.release();
     }
-
 })
+
+router.get('/userUpdate',(req,res)=>{
+    res.render('admin/user_update')
+})
+
+router.post('/userUpdate',(req,res)=>{
+    res.redirect('admin/user_list')
+})
+
+router.get('/userView',(req,res)=>{
+
+    res.render('admin/user_view')
+})
+
+
 
 router.get('/boardView',(req,res)=>{
 
@@ -47,24 +114,5 @@ router.get('/boardUpdate',(req,res)=>{
 router.post('/boardDelete',(req,res)=>{
     res.redirect('admin/board_list')
 })
-
-router.get('/userList',(req,res)=>{
-    res.render('admin/user_list')
-})
-
-router.get('/userUpdate',(req,res)=>{
-    res.render('admin/user_update')
-})
-
-router.get('/userView',(req,res)=>{
-    res.render('admin/user_view')
-})
-
-router.post('/userUpdate',(req,res)=>{
-    res.redirect('admin/user_list')
-})
-
-
-
 
 module.exports = router
