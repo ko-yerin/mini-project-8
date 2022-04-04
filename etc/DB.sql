db888106e387858a686441ff5c14590378ba4ab3
ko-yerin <rhdpfls12@naver.com>
2022-04-04 12:34:34 +0900

    db 레벨추가,  보드 delete다시 get으로

--- etc/DB.sql
+++ etc/DB.sql
@@ -11,7 +11,8 @@ CREATE TABLE user(
     gender CHAR(2) NOT NULL,  //이거아님 이거로 고정적으로 들어가니 CHAR로 해주면 될듯
     tell VARCHAR(11) NOT NULL,    //숫자가 아닌 문자 
     email VARCHAR(30) NOT NULL,
-    address VARCHAR(50) NOT NULL
+    address VARCHAR(50) NOT NULL,
+    level INT NOT NULL DEFAULT 3
 );
 
 CREATE TABLE board(
