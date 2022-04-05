f64c1140277ec4b8c7cb9d79c9c4d467a032f088
ko-yerin <rhdpfls12@naver.com>
2022-04-05 12:45:00 +0900

    멀고친거지  풀을 위한 커밋

--- routes/user/index.js
+++ routes/user/index.js
@@ -206,6 +206,7 @@ router.post('/update', async (req, res) => {
     //html에서 쿼리에 유저아디를 넣어줬지만 안됬다..
     //그래서 session의 유저아디를 넣어줬더니 성공
     //근데 왜 전부다 수정되는거지...
+    //그래서 update문뒤에  WHERE userid = '${userid}' 추가해줬더니 하나만 수정됨
     const {nickname, address, email, tell, userpw} = {...req.body}
     try{
     const sql = `UPDATE user set nickname = '${nickname}', address = '${address}', email = '${email}', tell = '${tell}', userpw = '${userpw}' WHERE userid = '${userid}';`
